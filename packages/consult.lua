d129ff5ece56226257c25284e9bb71856b637e88
njeromin <njeromin11@gmail.com>
2024-01-05 18:14:30 +1100

    feat(consult): add post install script

--- packages/consult.lua
+++ packages/consult.lua
@@ -1,5 +1,15 @@
 local pkg = require("lapis.pkg.builder").fromGithubRelease("1Turtle", "CONSULT", "1.4.3", "cosu.lua")
 
+pkg.wrappers = {
+	["cosu.lua"] = {
+		require("lapis.compat.craftos").call,
+	},
+}
+
 pkg.install_to = require("lapis.dirs").applications
+
+pkg.post_install = function(files)
+	files["cosu.lua"] = 'require("lapis.compat.craftos").call(function()\n' .. files["cosu.lua"] .. "\nend)"
+end
 
 return pkg
