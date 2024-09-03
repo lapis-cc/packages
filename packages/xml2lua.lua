8dc611804ef1cd970e313dd4c094eebd57ea55f4
njeromin <njeromin11@gmail.com>
2024-09-03 12:27:58 +1000

    feat: Add xml2lua

---
+++ packages/xml2lua.lua
@@ -0,0 +1,25 @@
+return {
+  src = pkgs.fromGithub("manoelcampos", "xml2lua", "054dea34d77bc53762331d1cc5026c66d87de768"),
+  install_to = "/lib/xml2lua/",
+  entrypoint = "init.lua",
+  ignored_paths = {
+    "Makefile",
+    "testxml.lua",
+    "test.Dockerfile",
+    "docker-compose.yml",
+    "books.xml",
+    "_config.xml",
+    "conversion-ways.png",
+    ".gitignore",
+    ".luacheckrc",
+    "xml2lua-1.6-2.rockspec",
+  },
+  ignored_patterns = {
+    "^example.*%.lua",
+    "^spec/.*",
+    "^%.github/.*",
+  },
+  path_mappings = {
+    ["xml2lua.lua"] = "init.lua",
+  },
+}
